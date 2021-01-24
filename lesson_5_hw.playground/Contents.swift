906b1f9c7e4cd81db4ccfaa0b55fbab4984d25c0
Ovanes <o-zakharyan@mail.ru>
2021-01-24 16:59:59 +0300

    final

--- lesson_5_hw.playground/Contents.swift
+++ lesson_5_hw.playground/Contents.swift
@@ -1,5 +1,8 @@
 import UIKit
 
+
+
+
 enum Actions {
     case openWindow
     case closeWindow
@@ -12,31 +15,108 @@ enum Actions {
 }
 
 protocol Carable {
-//    var isOpenWindows: Bool { get }
-//    var isOpenDoors: Bool { get }
-    func performActions(_ actions: Actions)
+    var isOpenWindows: Bool { get set }
+    var isOpenDoors: Bool { get set }
+    mutating func performActions(_ actions: Actions)
 }
 
 extension Carable {
-    func performActions(_ actions: Actions) {
-        print(actions)
+   mutating func performActions(_ actions: Actions) {
+        switch actions {
+        case.openWindow:
+            isOpenWindows = true
+        case.closeWindow:
+            isOpenWindows = false
+        default:
+            print("...")
+        }
     }
 }
 
 
 class SportCar: Carable {
-    var isEngineStarted: Bool = false
+    var isOpenWindows: Bool
     
-    init(isEngineStarted: Bool) {
+    var isOpenDoors: Bool
+    
+    var isEngineStarted: Bool = true
+
+    init(isEngineStarted: Bool, isOpenWindows: Bool, isOpenDoors: Bool) {
         self.isEngineStarted = isEngineStarted
+        self.isOpenWindows = isOpenWindows
+        self.isOpenDoors = isOpenDoors
+    }
+    func performActions(_ actions: Actions) {
+        switch actions {
+        case.startEngine:
+            isEngineStarted = true
+        case.stopEngine:
+            isEngineStarted = false
+        default:
+            break
+
+        }
+    }
+    func printProperties() {
+        print("is engine state", isEngineStarted)
     }
 }
+
 
 
 class TrunkCar: Carable {
-    var isOpenTrunk: Bool = false
+    var isOpenWindows: Bool
     
-    init(isOpenTrunk: Bool) {
+    var isOpenDoors: Bool
+    
+    var isOpenTrunk: Bool = false
+
+    init(isOpenTrunk: Bool, isOpenWindows: Bool, isOpenDoors: Bool) {
         self.isOpenTrunk = isOpenTrunk
+        self.isOpenWindows = isOpenWindows
+        self.isOpenDoors = isOpenDoors
+    }
+    func performActions(_ actions: Actions) {
+        switch actions {
+        case.openTrunk:
+            isOpenTrunk = true
+        case.closeTrunk:
+            isOpenTrunk = false
+        default:
+            break
+
+        }
     }
 }
+
+
+extension SportCar: CustomStringConvertible {
+    var description: String {
+        return "sport car data"
+    }
+}
+
+
+extension TrunkCar: CustomStringConvertible {
+    var description: String {
+        return "trunk car data"
+    }
+}
+
+
+var car = SportCar(isEngineStarted: true, isOpenWindows: true, isOpenDoors: false)
+var car1 = TrunkCar(isOpenTrunk: false, isOpenWindows: false, isOpenDoors: true)
+
+
+car.performActions(.startEngine)
+car1.performActions(.openTrunk)
+
+
+print(car)
+print(car1)
+
+
+
+
+
+
